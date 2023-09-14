e0617312d3b170f45e2eb69eda8d437f4fd7b07e
Nika Chekulaeva <nika.702@mail.ru>
2023-09-14 19:28:54 +0300

    All tests passed

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -81,7 +81,7 @@ class MainKtTest {
         val limitMastMaesMonthly = 600_000
 
         val result = countComission("Mastercard", 650000, 100000)
-        assertEquals("0.0", result)
+        assertEquals("превышен лимит", result)
     }
 
     @Test
